14a593161c738e8731de9f71d1f057a25bc6b625
wkdrl03 <56643338+wkdrl03@users.noreply.github.com>
2022-06-27 19:10:40 +0900

    웹사이트

---
+++ app.js
@@ -0,0 +1,9 @@
+const player = {
+    name :"nico",
+    sayHello:function(otherPersonName){
+        console.log("hello "+otherPersonName+" nice to meet you");
+    },
+}
+
+console.log(player.name);
+player.sayHello("lynn");
